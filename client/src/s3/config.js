9eb8df690030254dd64f02c8ae43f0eddc2e56a6
Danish Arora <35004822+danisharora099@users.noreply.github.com>
2020-12-13 23:24:44 +0530

    Delete config.js

--- client/src/s3/config.js
+++
@@ -1,6 +0,0 @@
-export const s3Config = {
-  bucketName: "testing213",
-  region: "ap-south-1",
-  accessKeyId: "AKIAJGV5PKNVVO6QNZLA",
-  secretAccessKey: "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"
-};
