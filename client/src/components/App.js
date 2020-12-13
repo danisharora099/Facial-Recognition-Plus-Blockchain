0381e274f886b3887d0c94bc08702312233557f1
lakshitmadaan20 <51285500+lakshitmadaan20@users.noreply.github.com>
2020-12-13 22:52:33 +0530

    table

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Upload from "./Upload";
 import Home from "./Home";
+import Table from './Table'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Upload} />
         <Route path="/addUser" exact component={Home} />
+        <Route path='/database' exact component={Table} />
       </Switch>
     </Router>
   );
---
+++ client/src/components/Table.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import CollegeInstance from "./college";
+
+export const Table = () => {
+
+    const [students, setStudents] = useState([]);
+    const [studentsCount, setStudentsCount] = useState(0)
+
+    useEffect(() => {
+        const studentCount = await CollegeInstance.methods.studentCount().call();
+        setStudentsCount(studentCount)
+        // Load students
+        for (var i = 1; i <= studentsCount; i++) {
+            const student = await CollegeInstance.methods.students(i).call();
+            setStudents(student);
+            console.log(students)
+        }
+        },[])
+
+    return (
+        <div>
+            <h2>Student Details</h2>
+        <br/>
+        <div class="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Sno.</th>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Class</th>
+              <th scope="col">RollNo</th>
+            </tr>
+          </thead>
+          <tbody>
+            { students.map((s, key) => {
+              return(
+                <tr key={key}>
+                  <th scope="row">{s.id.toString()}</th>
+                  <td>
+                    <img
+                      src={s.imageHash}
+                      className="img-thumbnail"
+                    />  
+                  </td>
+                  <td>{s.nameHash}</td>
+                  <td>{s.classHash}</td>
+                  <td>{s.rollnoHash}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+       </div>
+        </div>
+    )
+}
